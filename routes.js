3aaf51a22bdabd9f37be0efdeacd13272d0b6987
Nicholas Tallents <nicktallents11@gmail.com>
2018-01-04 20:12:18 -0500

    Initial Commit

---
+++ routes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Requires
+var fs = require('fs'); 
+var _ = require('lodash');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// module exports
+module.exports = {
+    
+}
+
+// API endpoints
+
+// function functionName(req, res) {}
